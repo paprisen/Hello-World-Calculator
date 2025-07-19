670cf106cddde4dbb94173978513a999da53fcad
Papri <papri.sen2020@gmail.com>
2025-07-19 21:07:02 +0530

    Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,23 @@
+//PAPRI SEN
+//24070123070
+//A3
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1,num2,sum=0,sub=0,mul=0,divi=0;
+    cout <<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    sum=num1+num2;
+    cout<<" Sum of given nos. is: "<<sum<<endl;
+    sub=num1-num2;
+    cout<<" Subtration of given nos. is: "<<sub;
+    mul=num1*num2;
+    cout<<"\n Multiplication of given nos. is: "<<mul;
+    divi=num1/num2;
+    cout<<"\n Division of given nos. is: "<<divi;
+    return 0;
+}
